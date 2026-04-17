09319e68e9f0cd40b33995f58de92cffd0a9be9f
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-17 17:42:29 +0000

    Automated update

--- src/Soenneker.Spotify.OpenApiClient/Models/SimplifiedEpisodeObject.cs
+++ src/Soenneker.Spotify.OpenApiClient/Models/SimplifiedEpisodeObject.cs
@@ -9,18 +9,18 @@ namespace Soenneker.Spotify.OpenApiClient.Models
 {
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
-    public partial class SimplifiedChapterObject : global::Soenneker.Spotify.OpenApiClient.Models.ChapterBase, IParsable
+    public partial class SimplifiedEpisodeObject : global::Soenneker.Spotify.OpenApiClient.Models.EpisodeBase, IParsable
     #pragma warning restore CS1591
     {
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.Spotify.OpenApiClient.Models.SimplifiedChapterObject"/></returns>
+        /// <returns>A <see cref="global::Soenneker.Spotify.OpenApiClient.Models.SimplifiedEpisodeObject"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static new global::Soenneker.Spotify.OpenApiClient.Models.SimplifiedChapterObject CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static new global::Soenneker.Spotify.OpenApiClient.Models.SimplifiedEpisodeObject CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
-            return new global::Soenneker.Spotify.OpenApiClient.Models.SimplifiedChapterObject();
+            return new global::Soenneker.Spotify.OpenApiClient.Models.SimplifiedEpisodeObject();
         }
         /// <summary>
         /// The deserialization information for the current model
