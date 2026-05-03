b6c3f7560b20e256b714e240a1529304fdeeb121
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-05-03 17:40:44 +0000

    Automated update

--- src/Soenneker.Spotify.OpenApiClient/Models/SimplifiedChapterObject.cs
+++ src/Soenneker.Spotify.OpenApiClient/Models/SimplifiedChapterObject.cs
@@ -9,26 +9,18 @@ namespace Soenneker.Spotify.OpenApiClient.Models
 {
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
-    public partial class PagingSimplifiedChapterObject : global::Soenneker.Spotify.OpenApiClient.Models.PagingObject, IParsable
+    public partial class SimplifiedChapterObject : global::Soenneker.Spotify.OpenApiClient.Models.ChapterBase, IParsable
     #pragma warning restore CS1591
     {
-        /// <summary>The items property</summary>
-#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
-#nullable enable
-        public List<global::Soenneker.Spotify.OpenApiClient.Models.SimplifiedChapterObject>? Items { get; set; }
-#nullable restore
-#else
-        public List<global::Soenneker.Spotify.OpenApiClient.Models.SimplifiedChapterObject> Items { get; set; }
-#endif
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.Spotify.OpenApiClient.Models.PagingSimplifiedChapterObject"/></returns>
+        /// <returns>A <see cref="global::Soenneker.Spotify.OpenApiClient.Models.SimplifiedChapterObject"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static new global::Soenneker.Spotify.OpenApiClient.Models.PagingSimplifiedChapterObject CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static new global::Soenneker.Spotify.OpenApiClient.Models.SimplifiedChapterObject CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
-            return new global::Soenneker.Spotify.OpenApiClient.Models.PagingSimplifiedChapterObject();
+            return new global::Soenneker.Spotify.OpenApiClient.Models.SimplifiedChapterObject();
         }
         /// <summary>
         /// The deserialization information for the current model
@@ -38,7 +30,6 @@ namespace Soenneker.Spotify.OpenApiClient.Models
         {
             return new Dictionary<string, Action<IParseNode>>(base.GetFieldDeserializers())
             {
-                { "items", n => { Items = n.GetCollectionOfObjectValues<global::Soenneker.Spotify.OpenApiClient.Models.SimplifiedChapterObject>(global::Soenneker.Spotify.OpenApiClient.Models.SimplifiedChapterObject.CreateFromDiscriminatorValue)?.AsList(); } },
             };
         }
         /// <summary>
@@ -49,7 +40,6 @@ namespace Soenneker.Spotify.OpenApiClient.Models
         {
             if(ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
             base.Serialize(writer);
-            writer.WriteCollectionOfObjectValues<global::Soenneker.Spotify.OpenApiClient.Models.SimplifiedChapterObject>("items", Items);
         }
     }
 }
